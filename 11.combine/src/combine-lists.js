2412616984060328143c9a1b78e681449197229b
Milos Aksentijevic <milos.aksentijevic@quantox.com>
2017-11-20 16:12:23 +0100

    Finished 11 combin

--- 11.combine/src/combine-lists.js
+++ 11.combine/src/combine-lists.js
@@ -5,10 +5,24 @@ var List = function() {
   'use strict';
 
   function combine(list1, list2) {
+    let result = [];
+    let size = (list1.length + list2.length);
+    for(let i = 0; i < size; i++){
+      if(list1.length > 0){result.push(list1.shift());}
+      if(list2.length > 0){result.push(list2.shift());}
+    }
 
+    return result;
   }
 
   return {
     combine: combine
   };
 };
+
+
+var list1 = [1, 2, 3, 4, 5, 6, 7];
+var list2 = ['a', 'b', 'c'];
+
+
+var result = [1, 'a', 2, 'b', 3, 'c', 4, 5, 6, 7];
