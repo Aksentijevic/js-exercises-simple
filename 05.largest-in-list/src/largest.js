fb8db4418404e5350b77a4621b8c0ac344836920
Milos Aksentijevic <milos.aksentijevic@quantox.com>
2017-11-20 13:01:27 +0100

    Finished 05 largest in list

--- 05.largest-in-list/src/largest.js
+++ 05.largest-in-list/src/largest.js
@@ -5,7 +5,19 @@ var List = function() {
   'use strict';
 
   function findLargest(list) {
+    let largest;
 
+    if(list.length > 0){
+      largest= list[0]
+      for(let i=1; i < list.length; i++){
+        if(list[i] >= largest ){
+          largest = list[i];
+        }
+      }
+    } else {
+      throw 'List cannot be empty.';
+    }
+    return largest
   }
 
   return {
