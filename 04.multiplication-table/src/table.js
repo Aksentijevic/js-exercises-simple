ad6b80853583011eaee48b0b296fa108dba334e0
Milos Aksentijevic <milos.aksentijevic@quantox.com>
2017-11-20 12:50:08 +0100

    Finished 04 multiplication-table

--- 04.multiplication-table/src/table.js
+++ 04.multiplication-table/src/table.js
@@ -6,7 +6,23 @@ var MultiplicationTable = function() {
   'use strict';
 
   function generate5() {
+    let table = [];
+    let n = 5;
 
+    for(let i=0; i<=n; i++) {
+      table[i] = [];
+        table[i][0] = i;
+    }
+    for(let i=1; i<=n; i++) {
+        table[0][i] = i;
+    }
+    for(let i=1; i<=n; i++) {
+        for(let j=1; j<=n; j++) {
+          table[i][j] = i*j;
+      }
+    }
+
+    return table;
   }
 
   return {
