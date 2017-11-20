8e0d4ecfe1fb24ec3a4d44645a9765dbbeee2c15
Milos Aksentijevic <milos.aksentijevic@quantox.com>
2017-11-20 11:37:50 +0100

    Finished 03 sum-product

--- 03.sum-product/src/sum-product.js
+++ 03.sum-product/src/sum-product.js
@@ -7,7 +7,24 @@ var SumProduct = function() {
   'use strict';
 
   function calculate(n, opts) {
+    let result = 0;
 
+    if (opts.mode == 'sum'){
+
+      for(let i = 1; i <= n; i++){
+        result += i;
+      }
+
+    } else if (opts.mode == 'product'){
+        result = 1;
+        for(let i = n; i >= 1; i--){
+            result *= i;
+        }
+    } else if (opts.mode == 'math is hard') {
+        throw 'Invalid calculation mode.';
+    }
+
+    return result;
   }
 
   return {
